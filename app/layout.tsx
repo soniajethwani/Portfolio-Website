f75212b83a24eabbb76ec852a195d320183b9df2
Sonia Jethwani <sonia@macbook-pro.myfiosgateway.com>
2023-08-11 16:50:28 -0400

    footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import Header from "@/components/header"
 import ActiveSectionContextProvider from '@/context/active-section-context'
 import { Toaster } from 'react-hot-toast'
+import Footer from '@/components/footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -26,7 +27,7 @@ export default function RootLayout({
           {/* Note: client components can still have server components wrapped inside. */}
           <Header />
           {children}
-
+          <Footer />
           <Toaster position='bottom-right'/>
         </ActiveSectionContextProvider>
       </body>
